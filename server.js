9dab4f5d4ebce4eb013ebc21d96cae2db83f6d9e
Sameh <samehothman20162017@gmail.com>
2021-10-17 22:52:59 +0200

    add API route to fetch user stats

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const request = require('request');
 const cors = require('cors');
-const keys = require('./keys');
+const { API_HEADER, API_KEY, URL } = require('./keys');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.get('/api/:id', (req, res, next) => {
+  var options = {
+    method: 'GET',
+    url: URL(req.params.id),
+    headers: {
+      API_HEADER: API_KEY,
+    },
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    res.status(200).json(JSON.parse(response.body));
+  });
+});
 
 app.listen(3001, () => {
-    console.log('express server on port ', 3001);
-   });
-   
+  console.log('express server on port ', 3001);
+});
