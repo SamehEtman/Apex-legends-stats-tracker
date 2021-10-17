cfec79953e7c6936355e5838473821ba954e69de
Sameh <samehothman20162017@gmail.com>
2021-10-17 21:08:38 +0200

    add LegendItem format

--- src/components/LegendItem.js
+++ src/components/LegendItem.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React from 'react';
 
-const LegendItem = ()=>{
-    return (<div>
-        Legend Item
-    </div>)
-}
+const LegendItem = () => {
+  return (
+    <div className="item">
+      <img className="ui image" src="" alt="" />
+      <div className="content">
+        <div className="header">Text Holder</div>
+        <div className="description">Text Holder</div>
+      </div>
+    </div>
+  );
+};
 export default LegendItem;
