4103f088aa7dd78cec8a60dcf9899310f624d7db
Sameh <samehothman20162017@gmail.com>
2021-10-18 02:39:14 +0200

    add changes for prod env

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.get('/api/:id', (req, res, next) => {
   var options = {
     method: 'GET',
-    url: URL(req.params.id),
+    url: URL + req.params.id + `/segments/legend`,
     headers: {
       'TRN-Api-Key': API_KEY,
     },
@@ -20,7 +20,14 @@ app.get('/api/:id', (req, res, next) => {
     res.status(200).json(JSON.parse(response.body));
   });
 });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 app.listen(3001, () => {
   console.log('express server on port ', 3001);
 });
